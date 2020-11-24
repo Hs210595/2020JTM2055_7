8ea5116018bc8a06336e5d4f5003da14aab7b0a2
Hs210595 <hrishikeshsaste@gmail.com>
2020-11-24 12:54:59 +0530

    Logic to genrate random number

---
+++ ps2.py
@@ -0,0 +1,6 @@
+import random
+print("Enter the range between you want to play the game")
+lower_bound=int(input())
+upper_bound=int(input())
+number=random.randint(lower_bound,upper_bound)
+print(number)
