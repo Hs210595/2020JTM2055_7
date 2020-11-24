51a339500ee9a55e08d25350891da12235e15125
Hs210595 <hrishikeshsaste@gmail.com>
2020-11-24 15:29:49 +0530

    Final Code

--- ps1.py
+++ ps1.py
@@ -15,8 +15,27 @@ class lab:
         print('{0}({1}) at {2}'.format(self.name,self.role,self.email))
 
 
-s1=lab('Hrishi','jtm202055@iitd.ac.in','S')
-s1.print_info()
+class software_lab(lab):
+
+    def __init__(self,name,role,email,scored_marks):
+        super(software_lab,self).__init__(name,email,role)
+        self.marks=scored_marks
+
+    def print_marks(self):
+        if self.role=='S':
+            print(' {0}(S) scored {1} '.format(self.name,self.marks))
+        else:
+            print(' {0}(I) can be reached at {1} '.format(self.name,self.email))
+
+s1=software_lab('Hrishi','S','dfbajb@gmai.com',40)
+s1.print_marks()
+i1=software_lab('Vivek','I','vivek@cc.com',50)
+i1.print_marks()
+
+
+
+
+
 
 
     
